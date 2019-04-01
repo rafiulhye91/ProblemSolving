52e5c438dced128c4c54ecb0011d1b93c2ab1694
Rafiul Hye <rafiulhye91@gmail.com>
2019-04-01 11:41:53 -0500

    Given a sorted array and a target value find inserting index
    - return the index if the target is found
    - return the index where it would be if the targets is not found

---
+++ ProblemSolving/ArrayProblems/InsertPosition.java
@@ -0,0 +1,57 @@
+/*
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Example 1:
+
+Input: [1,3,5,6], 5
+Output: 2
+Example 2:
+
+Input: [1,3,5,6], 2
+Output: 1
+Example 3:
+
+Input: [1,3,5,6], 7
+Output: 4
+Example 4:
+
+Input: [1,3,5,6], 0
+Output: 0
+ */
+
+class InsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int size = nums.length;
+        return binarySearch(nums, 0, size-1, target);
+    }
+
+    private int binarySearch(int[] nums,int left, int right, int target){
+
+        int mid = left+(right-left)/2;
+        if(left>=right){
+            if(nums[mid]>target){
+                return mid;
+            }
+            if(nums[mid]<target)
+                return mid+1;
+        }
+        if(nums[mid]==target){
+            return mid;
+        }
+
+        if(nums[mid]>target){
+            return binarySearch(nums, left, mid, target);
+        }
+
+        if(nums[mid]<target){
+            return binarySearch(nums, mid+1, right, target);
+        }
+
+        if(nums[mid]<target){
+            return mid+1;
+        }
+        return mid;
+    }
+}
